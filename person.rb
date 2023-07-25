424032d03298b7329e6cfcee080f279b4386e685
gersonhiggins <gahg.10gerson@gmail.com>
2023-07-25 16:05:58 -0400

    add methods

--- person.rb
+++ person.rb
@@ -1,16 +1,29 @@
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
+require_relative 'nameable'
 
-  def initialize(name = 'Unknown', age = nil, parent_permission: true)
+class Person < Nameable
+  attr_reader :id, :name, :age
+
+  def initialize(age, name = "Unknown", parent_permission = true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
